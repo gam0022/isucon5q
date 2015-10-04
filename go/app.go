5b526e9108e346b066cdfe4de2a61f154c48b220
gam0022 <gam0022@gmail.com>
2015-10-04 17:44:02 +0900

    entries の LIMIT 1000 の部分をEXISTSで最適化(3804)

--- go/app.go
+++ go/app.go
@@ -350,7 +350,7 @@ LIMIT 10`, user.ID)
 	}
 	rows.Close()
 
-	rows, err = db.Query(`SELECT * FROM entries ORDER BY created_at DESC LIMIT 1000`)
+	rows, err = db.Query(`SELECT * FROM entries AS E WHERE EXISTS(SELECT id FROM relations AS R WHERE R.one = ? AND R.another = E.user_id) ORDER BY created_at DESC LIMIT 10`, user.ID)
 	if err != sql.ErrNoRows {
 		checkErr(err)
 	}
@@ -360,9 +360,6 @@ LIMIT 10`, user.ID)
 		var body string
 		var createdAt time.Time
 		checkErr(rows.Scan(&id, &userID, &private, &body, &createdAt))
-		if !isFriend(w, r, userID) {
-			continue
-		}
 		entriesOfFriends = append(entriesOfFriends, Entry{id, userID, private == 1, strings.SplitN(body, "\n", 2)[0], strings.SplitN(body, "\n", 2)[1], createdAt})
 		if len(entriesOfFriends) >= 10 {
 			break
